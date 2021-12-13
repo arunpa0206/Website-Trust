44799aa9557aaf3ac3a6b3dc696935ffa4f0f0db
sujithgnair <sujithgnair@gmail.com>
2021-12-13 10:08:59 +0530

    no message

--- wp-content/themes/social-care-lite/woocommerce.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<div class="container">
-    	<div id="sc_innerpage_wrap">
-			<?php woocommerce_content(); ?>
-		</div><!-- sc_innerpage_wrap -->
-</div><!-- container -->     
-<?php get_footer(); ?>
